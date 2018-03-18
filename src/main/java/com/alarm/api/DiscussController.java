79bc97aa857621d641c68e3d390aef496644619f
Dreamover Studio <dreamover@Don.local>
2018-03-18 21:38:47 +0800

    asdasd

--- src/main/java/com/alarm/api/DiscussController.java
+++ src/main/java/com/alarm/api/DiscussController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alarm.model.Discuss;
 import com.alarm.service.DiscussService;
 
 import net.sf.json.JSONObject;
@@ -30,6 +31,17 @@ public class DiscussController {
 	@ResponseBody
 	public String subject(@RequestParam("discuss_title") String discuss_title, @RequestParam("discuss_content") String discuss_content, @RequestParam("discuss_user_id") Integer discuss_user_id){
 		JSONObject retval = new JSONObject();
+		
+		Discuss discuss = new Discuss();
+		discuss.setTitle(discuss_title);
+		discuss.setContent(discuss_content);
+		discuss.setUserId(discuss_user_id);
+		
+		if( discussService.insert(discuss) == 1 ){
+			retval.put("status", true);
+		}else{
+			retval.put("status", false);
+		}
 		
 		return retval.toString();
 	}
